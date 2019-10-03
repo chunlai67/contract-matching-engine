93e4f2fbf87fd8192e7d40c10eb173da9cdb2f52
zhouchunlai <124033543@qq.com>
2019-10-03 13:02:06 +0800

    new proj

---
+++ cmake-build-debug/CMakeFiles/contract_matching_engine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/contract_matching_engine.dir/main.cpp.o"
+  "contract_matching_engine"
+  "contract_matching_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/contract_matching_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
